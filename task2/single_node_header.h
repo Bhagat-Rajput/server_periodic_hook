feafcf9c38271d276ff2ac1477827da85bb0662c
Bhagat-Rajput <bhagatsingh.rajput@altair.com>
2017-05-12 12:54:11 -0700

    data printing in python file

---
+++ task2/single_node_header.h
@@ -0,0 +1,12 @@
+struct node{
+        char attr_name[15];
+        char attr_value[20];
+        char resc_name[40];
+        struct node *next;
+        struct node *prev;
+};
+struct node* head,temp;
+struct node* get_node(char *attr_data[3]);
+void insert(char *attr_data[3]);
+void print_result();
+int file_read();
--- task2/single_node_struct.c
+++ task2/single_node_struct.c
@@ -2,15 +2,9 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
-struct node{
-	char attr_name[15];
-	char attr_value[20];
-	char resc_name[40];
-	struct node *next;
-	struct node *prev;
-};
+#include<unistd.h>
+#include"single_node_header.h"
 
-struct node* head;
 struct node* get_node(char *attr_data[3]) {
 	struct node* new_node = (struct node*)malloc(sizeof(struct node));
 	strcpy(new_node->attr_name,attr_data[0]);
@@ -33,7 +27,7 @@ void insert(char *attr_data[3]) {
 	new_node->prev = temp;
 }
 
-void print() {
+void print_result() {
 	struct node* temp = head;
 	while(temp != NULL) {
 		printf("Attr_Name = %s  Attr_Value = %s   Resc_Name = %s\n",temp->attr_name,temp->attr_value,temp->resc_name);
@@ -41,18 +35,19 @@ void print() {
 	}
 }
 
-int main(){
+int file_read(){
 	FILE *fp;
 	static char data [50];
 	char ch,*token;
 	char *attr_data[2];
-        const char *attr_names[6];
+        const char *attr_names[7];
 	attr_names[0] = "Mom";
 	attr_names[1] = "ntype";
 	attr_names[2] = "state";
 	attr_names[3] = "pcpus";
 	attr_names[4] = "resources_available.arch";
 	attr_names[5] = "resources_available.mem";
+	attr_names[6] = "resources_available.ncpus";
 	int i=0,per=0;
 	bool flag = true;
 	fp = fopen("pbsnodes.txt","r");
@@ -60,6 +55,7 @@ int main(){
 	{
 		printf("Could not open file");
 		return 1;
+	        	
 	}
         memset(&data[0], 0, sizeof(data));
         while ((ch = getc(fp)) != EOF){
@@ -144,6 +140,17 @@ int main(){
 						attr_data[0] = "NULL";
 						insert(attr_data);
 					}
+				}
+				else if(strncmp(data,attr_names[6],24)== 0){
+					if(per == 0){
+						attr_data[2] = token;		
+						per++;
+					}
+					else{
+						attr_data[1] = token;
+						attr_data[0] = "NULL";
+						insert(attr_data);
+					}
 				}	
 	 			token = strtok(NULL,"=");	
 			}
@@ -153,7 +160,22 @@ int main(){
                 }
         }
         fclose(fp);
-print();
-printf("\nFile Closed\n");
 return 0;
+}
+
+int main(){
+file_read();
+int x;
+pid_t pid = fork();
+        if(pid==0){
+                printf("Child process called\n");
+                x = execl("./python_file.py", "python_file.py", NULL);
+                if(x==-1){
+                        perror("./python_file.py");
+                }
+        }
+        else{
+                wait();
+                printf("Parent Process called\n");
+        }
 }
